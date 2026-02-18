aa4facf95c577c5a71d720a2a74b5deb0ac80781
Sohaib Arshid <sohaibali1277@gmail.com>
2026-02-18 20:59:11 +0500

    Add axios lib and proxy in Frontend

--- chapter-2/Frontend/src/App.jsx
+++ chapter-2/Frontend/src/App.jsx
@@ -1,11 +1,31 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios';
 import './App.css'
 
 function App() {
+  const [jokes , setJokes] = useState([]);
+  useEffect(()=>{
+    axios.get('/api/jokes')
+    .then((response) =>{
+      setJokes(response.data)
+    })
+    .catch((error)=>{
+      console.log(error);
+    })
+  })
   return (
     <>
     <h1>
       hello world
+      <p>Jokes : {jokes.length} </p>
+      {
+        jokes.map((Jokes , index)=>{
+          <div key={Jokes.id}>
+            <h1>{Jokes.title}</h1>
+            <p>{Jokes.content}</p>
+          </div>
+        })
+      }
     </h1>
     </>
   )
