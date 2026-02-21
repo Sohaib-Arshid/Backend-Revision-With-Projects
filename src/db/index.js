450fb8761e3cd4d375f1c01a31f93781ebb50f56
Sohaib Arshid <sohaibali1277@gmail.com>
2026-02-21 11:46:56 +0500

    Database connection and setup env

---
+++ src/db/index.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constans.js";
+
+export const connectdb = async () => {
+    try {
+       const connectiondb = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+       console.log(`Database are connectd !! HOST ${connectiondb.connection.host}`);
+    } catch (error) {
+        console.error("Error", error)
+        process.exit(1)
+    }
+}
+
+export default connectdb
