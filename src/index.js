b7931c7abc340eb60980c69e166584814af7d340
Sohaib Arshid <sohaibali1277@gmail.com>
2026-02-22 10:42:33 +0500

    etup Express server and initialize app with DB connection

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import connectdb from "./db/index.js";
 import dotenv from "dotenv"
-dotenv.config({path : "./env"})
+dotenv.config({ path: "./env" })
 
 connectdb()
+    .then(() => {
+        const server = app.listen(process.env.PORT || 3000, () => {
+            console.log(`server is listing on ${process.env.PORT}`);
+        })
+        server.on("error", (error) => {
+            console.log("Error in port", error);
+
+        })
+    }).catch((error) => {
+        console.log("Mongodb connection failed !! ", error);
+
+    })  
