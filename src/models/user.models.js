355a8e94f995f7a5a94c3f7b5631f79be790c4c4
Sohaib Arshid <sohaibali1277@gmail.com>
2026-02-28 15:37:09 +0500

    define user and video Schema and models

---
+++ src/models/user.models.js
@@ -0,0 +1,50 @@
+import mongoose, { Schema } from "mongoose";
+
+const UserSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        index: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    fullname: {
+        type: String,
+        required: true,
+        trim: true,
+        index: true,
+    },
+    avatar: {
+        type: String,
+        required: true
+    },
+    coverImage: {
+        type: String
+    },
+    watchHistory: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Video"
+        }
+    ],
+    password: {
+        type: String,
+        required: [true, "Password is required"]
+    },
+    refrestoken: {
+        type: String,
+    },
+
+},
+    {
+        timestamps: true
+    }
+)
+
+const User = mongoose.model("User", UserSchema) 
