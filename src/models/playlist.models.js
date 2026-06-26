815f56c85925026c7c0018638cea7e9b5fd50df4
Sohaib Arshid <sohaibali1277@gmail.com>
2026-06-26 12:10:56 +0500

    add playlist model with schema definition

---
+++ src/models/playlist.models.js
@@ -0,0 +1,35 @@
+import mongoose, { model, Schema } from "mongoose";
+
+const playlistSchema = new Schema({
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    },
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        index: true,
+    },
+    description: {
+        type: String,
+        trim: true,
+        index: true,
+    },
+    video: {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+    },
+    isPublic : {
+        type : Boolean,
+        default : true,
+        required : true
+    }
+
+},
+    {
+        timestamps: true
+    }
+)
+
+const Playlist = mongoose.model("Playlist" , playlistSchema)
